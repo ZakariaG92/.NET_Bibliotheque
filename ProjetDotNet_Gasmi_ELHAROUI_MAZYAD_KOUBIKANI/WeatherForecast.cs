d36039dd629fed554fb7ae0c8f088550df79522e
Zakaria GASMI <zakaria.gasmi@etu.unice.fr>
2021-02-09 14:54:09 +0100

    Base du projet

---
+++ ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
