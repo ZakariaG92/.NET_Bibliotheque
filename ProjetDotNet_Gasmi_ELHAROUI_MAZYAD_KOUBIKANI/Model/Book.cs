e687548145d040e6c2937cc124bf520c267695ae
Zakaria Gasmi <zakaria.gasmi@etu.univ-cotedazur.fr>
2021-02-09 16:25:11 +0100

    creation de classes

---
+++ ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI/Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI.Model
+{
+    public class Book
+    {
+        [JsonProperty("id")]
+        String Id { get; set; }
+
+        [JsonProperty("title")]
+        String Title { get; set; }
+        
+        [JsonProperty("contenu")]
+        String Contenu { get; set; }
+
+        [JsonProperty("prix")]
+        float Prix { get; set; }
+
+        [JsonProperty("genre")]
+        List<Genre> Genre { get; set; }
+
+    }
+}
---
+++ ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI/Model/Genre.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI.Model
+{
+    public class Genre
+    {
+        [JsonProperty("id")]
+        String Id { get; set; }
+
+        [JsonProperty("nom")]
+        String Nom { get; set; }
+
+    }
+}
