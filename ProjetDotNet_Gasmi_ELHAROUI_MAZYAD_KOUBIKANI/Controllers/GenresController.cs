c6536c54052ac1611d9dfd6d1a89aad95a5e8fa2
Hassan Mazyad <Hassan.Mazyad@outlook.com>
2021-02-10 15:49:51 +0100

    api get genres

---
+++ ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI/Controllers/GenresController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI.Model;
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver.Linq;
+using System.Text.Json;
+
+
+namespace ProjetDotNet_Gasmi_ELHAROUI_MAZYAD_KOUBIKANI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GenresController>/test
+        [HttpGet("id/{id}")]
+        public string GetById(string id)
+        {
+            IMongoCollection<Genre> collection = MongoConn.getCollectionGenres();
+
+            var query =
+            from e in collection.AsQueryable<Genre>()
+            where e.Id == Int32.Parse(id)
+            select e;
+
+            string jsonString = JsonSerializer.Serialize(query);
+
+            return jsonString;
+        }
+
+
+        // GET api/<GenresController>/test
+        [HttpGet("names/{nom}")]
+        public string GetByName(string nom)
+        {
+            IMongoCollection<Genre> collection = MongoConn.getCollectionGenres();
+
+            var query =
+            from e in collection.AsQueryable<Genre>()
+            where e.Nom == nom
+            select e;
+
+            List<Genre> listGenres = new List<Genre>();
+            foreach (var genres in query)
+            {
+                listGenres.Add(genres);
+            }
+
+            string jsonString = JsonSerializer.Serialize(listGenres);
+
+            return jsonString;
+        }
+    }
+}
